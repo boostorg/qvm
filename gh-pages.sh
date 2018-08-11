4a27a445bd2e0e111a319d8101fa8b663a8d7641
Emil Dotchevski <emildotchevski@gmail.com>
2018-08-10 18:21:17 -0700

    updating gh-pages.sh

--- gh-pages.sh
+++ gh-pages.sh
@@ -4,7 +4,7 @@ git add -u README.adoc
 git add -u index.html
 git commit -m "Documentation update"
 git push
-mv index.html index1.html
+cp index.html index1.html
 git checkout gh-pages
 rm index.html
 mv index1.html index.html
