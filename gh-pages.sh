8f6f1156809f603b9fb6e4d96344cbf39ffed7a7
Emil Dotchevski <emildotchevski@gmail.com>
2018-12-17 13:43:09 -0800

    documentation update

--- gh-pages.sh
+++
@@ -1,9 +0,0 @@
-set -e
-asciidoctor README.adoc -o index1.html
-git checkout gh-pages
-rm index.html
-mv index1.html index.html
-git add -u index.html
-git commit -m "documentation update"
-git push
-git checkout master
