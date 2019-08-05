7250d56bb81db52f750e1863527956a7c8a4dd8e
Emil Dotchevski <emildotchevski@gmail.com>
2019-08-04 19:11:06 -0700

    Fixing compile error in cmp_vv_test.cpp

--- test/cmp_vv_test.cpp
+++ test/cmp_vv_test.cpp
@@ -3,7 +3,6 @@
 //Distributed under the Boost Software License, Version 1.0. (See accompanying
 //file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/qvm/detail/equal_to.hpp>
 #include <boost/qvm/vec.hpp>
 #include <boost/qvm/vec_operations.hpp>
 #include <boost/qvm/vec_traits_array.hpp>
