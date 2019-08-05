79612faa2c285e3ad1e6ee347b44901493c4f17a
Emil Dotchevski <emildotchevski@gmail.com>
2019-08-05 12:27:01 -0700

    Added missing copyright messages

--- test/test_qvm_scalar.hpp
+++ test/test_qvm_scalar.hpp
@@ -1,3 +1,9 @@
+//Copyright (c) 2008-2016 Emil Dotchevski and Reverge Studios, Inc.
+//Copyright (c) 2019 agate-pris
+
+//Distributed under the Boost Software License, Version 1.0. (See accompanying
+//file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef UUID_F0B34CB08687439DA6046DBFB7C674DD
 #define UUID_F0B34CB08687439DA6046DBFB7C674DD
 
