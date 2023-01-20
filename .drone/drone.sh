6d50d19891eeaaf76b17292f3d268890466d0f37
Emil Dotchevski <emildotchevski@gmail.com>
2023-01-19 21:49:50 -0800

    Drone, GHA update

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -1,41 +1,24 @@
 #!/bin/bash
 
-# Copyright 2020 Rene Rivera, Sam Darwin
+# Copyright 2022 Peter Dimov
 # Distributed under the Boost Software License, Version 1.0.
-# (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+# https://www.boost.org/LICENSE_1_0.txt
 
-set -e
-export TRAVIS_BUILD_DIR=$(pwd)
-export DRONE_BUILD_DIR=$(pwd)
-export TRAVIS_BRANCH=$DRONE_BRANCH
-export VCS_COMMIT_ID=$DRONE_COMMIT
-export GIT_COMMIT=$DRONE_COMMIT
-export REPO_NAME=$DRONE_REPO
-export USER=$(whoami)
-export CC=${CC:-gcc}
-export PATH=~/.local/bin:/usr/local/bin:$PATH
+set -ex
 
-if [ "$DRONE_JOB_BUILDTYPE" == "boost" ]; then
+DRONE_BUILD_DIR=$(pwd)
 
-echo '==================================> INSTALL'
+BOOST_BRANCH=develop
+if [ "$DRONE_BRANCH" = "master" ]; then BOOST_BRANCH=master; fi
 
 cd ..
-git clone -b $TRAVIS_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
+git clone -b $BOOST_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
 cd boost-root
-git submodule update --init tools/build
-git submodule update --init tools/inspect
-git submodule update --init libs/config
 git submodule update --init tools/boostdep
-cp -r $TRAVIS_BUILD_DIR/* libs/qvm
-python tools/boostdep/depinst/depinst.py qvm
+cp -r $DRONE_BUILD_DIR/* libs/$LIBRARY
+python tools/boostdep/depinst/depinst.py $LIBRARY
 ./bootstrap.sh
-./b2 headers
-cd libs/qvm
-
-echo '==================================> SCRIPT'
+./b2 -d0 headers
 
 echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
-../../b2 test toolset=$TOOLSET cxxstd=$CXXSTD variant=debug,release ${UBSAN:+cxxflags=-fsanitize=undefined cxxflags=-fno-sanitize-recover=undefined linkflags=-fsanitize=undefined debug-symbols=on} ${LINKFLAGS:+linkflags=$LINKFLAGS}
-
-fi
-
+./b2 -j3 libs/$LIBRARY/test toolset=$TOOLSET cxxstd=$CXXSTD variant=debug,release ${ADDRMD:+address-model=$ADDRMD} ${UBSAN:+undefined-sanitizer=norecover debug-symbols=on} ${ASAN:+address-sanitizer=norecover debug-symbols=on} ${LINKFLAGS:+linkflags=$LINKFLAGS}
