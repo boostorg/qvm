e659dc47a6369504b7d607f3749de1e808887e61
Emil Dotchevski <emildotchevski@gmail.com>
2023-03-08 22:44:36 -0800

    translation() supports non-square matrices

--- test/translation_test.cpp
+++ test/translation_test.cpp
@@ -17,19 +17,19 @@
 
 namespace
     {
-    template <int D>
+    template <int R, int C>
     void
     test()
         {
         using namespace boost::qvm;
-        test_qvm::matrix<M1,D,D> x(42,1);
-        test_qvm::vector<V1,D-1> y=translation(x);
-        for( int i=0; i!=D-1; ++i )
-            y.b[i]=x.a[i][D-1];
+        test_qvm::matrix<M1,R,C> x(42,1);
+        test_qvm::vector<V1,C-1> y=translation(x);
+        for( int i=0; i!=C-1; ++i )
+            y.b[i]=x.a[i][C-1];
         BOOST_QVM_TEST_EQ(y.a,y.b);
         translation(x) *= 2;
-        for( int i=0; i!=D-1; ++i )
-            x.b[i][D-1] *= 2;
+        for( int i=0; i!=C-1; ++i )
+            x.b[i][C-1] *= 2;
         BOOST_QVM_TEST_EQ(x.a,x.b);
         translation(x) + translation(x);
         -translation(x);
@@ -39,8 +39,11 @@ namespace
 int
 main()
     {
-    test<3>();
-    test<4>();
-    test<5>();
+    test<3,3>();
+    test<2,3>();
+    test<4,4>();
+    test<3,4>();
+    test<5,5>();
+    test<4,5>();
     return boost::report_errors();
     }
