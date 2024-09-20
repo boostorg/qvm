c4b9c063a076644575fe9a685c02cd4c10d24405
Emil Dotchevski <emildotchevski@gmail.com>
2024-09-19 22:21:59 -0700

    Fix for issue 54

--- include/boost/qvm/quat_traits.hpp
+++ include/boost/qvm/quat_traits.hpp
@@ -79,7 +79,7 @@ typename enable_if_c<
     void>::type
 write_quat_element_idx( int i, Q & q, typename quat_traits<Q>::scalar_type s )
     {
-    quat_traits<Q>::template write_element_idx(i, q) = s;
+    quat_traits<Q>::write_element_idx(i, q) = s;
     }
 
 template <class Q>
@@ -89,7 +89,7 @@ typename enable_if_c<
     void>::type
 write_vec_element_idx( int i, Q & q, typename quat_traits<Q>::scalar_type s )
     {
-    quat_traits<Q>::template write_element_idx(i, q, s);
+    quat_traits<Q>::write_element_idx(i, q, s);
     }
 
 } }
